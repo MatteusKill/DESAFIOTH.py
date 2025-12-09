5b2643843d5dd6ef421b3626969308cc0cecd9ed
Matteus KIll <matteuskill@gmail.com>
2025-12-09 08:41:35 -0400

    ex22

--- EX22.py
+++ EX22.py
@@ -2,15 +2,17 @@ cores = {
     "limpa":"\033[m",
     "vermelho":"\033[31m"
 }
+try:
+    n1 = int(input("Informe um valor de 1 a 7: "))
 
-n1 = int(input("Informe um valor de 1 a 7: "))
+    diaSemana = {
+        1:"Domingo", 2:"Segunda-Feira", 3:"Terça-Feira",
+        4:"Quarta-Feira", 5:"Quinta-Feira",6:"Sexta-Feira",7:"Sábado"
+    }
 
-diaSemana = {
-    1:"Domingo", 2:"Segunda-Feira", 3:"Terça-Feira",
-    4:"Quarta-Feira", 5:"Quinta-Feira",6:"Sexta-Feira",7:"Sábado"
-}
-
-if n1 > 0  and n1 <= 7:
-    print(f"O dia da semana eh: {diaSemana[n1]}")
-else:
-    print(f"{cores['vermelho']}Erro! Digite um número válido.{cores['limpa']}")
+    if n1 > 0  and n1 <= 7:
+        print(f"O dia da semana eh: {diaSemana[n1]}")
+    else:
+        print(f"{cores['vermelho']}Erro! Digite um número válido.{cores['limpa']}")
+except ValueError:
+    print(f"{cores['vermelho']}Erro! Somente números inteiros.{cores['limpa']}")
